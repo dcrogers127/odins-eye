4e3914b397693328623bad5f35440328def38d76
Daniel Rogers <dcrogers127@gmail.com>
2019-10-26 12:43:46 -0700

    Add data

--- app/dao/GameDao.scala
+++ app/dao/GameDao.scala
@@ -7,9 +7,9 @@ import scala.util.Try
 
 class GameDao {
 
-  def getUsers: Try[Seq[Game]] = Try {
+  def getGames: Try[Seq[Game]] = Try {
     NamedDB('statsstore).readOnly { implicit session =>
-      sql"select * from users".map(Game.fromRS).list().apply()
+      sql"select * from games".map(Game.fromRS).list().apply()
     }
   }
 
--- app/model/Game.scala
+++ app/model/Game.scala
@@ -1,5 +1,7 @@
 package model
 
+import scalikejdbc.WrappedResultSet
+
 case class Game(
   game_date: String,
   visitor: String,
@@ -8,3 +10,10 @@ case class Game(
   home_pts: String
 )
 
+object Game {
+  def fromRS(rs: WrappedResultSet): Game = {
+    Game(rs.string("game_date"),
+      rs.string("visitor"), rs.string("visitor_pts"),
+      rs.string("home"), rs.string("home_pts"))
+  }
+}
