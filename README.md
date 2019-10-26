72ebeda75890fb16951190d59b324a4ebeccda2a
Daniel Rogers <dcrogers127@gmail.com>
2019-10-26 12:06:36 -0700

    Add data

--- README.md
+++ README.md
@@ -4,5 +4,12 @@ Base from sbt new denisftw/play-event-sourcing-starter.g8.
 Data from https://www.basketball-reference.com/leagues/NBA_2020_games.html
 
 
-## Commands to run outside of book.
+## Commands to run first time
+sudo psql -U postgres --command "CREATE USER scalauser WITH SUPERUSER PASSWORD 'scalapass';"
+sudo createdb -U postgres -O scalauser authdb
 sudo createdb -U postgres -O scalauser statsstoredb
+npm i
+
+## Commands to run web app
+npm run watch
+sbt run
---
+++ conf/evolutions/statsstore/1.sql
@@ -0,0 +1,29 @@
+# --- !Ups
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+Date,Start (ET),Visitor/Neutral,PTS,Home/Neutral,PTS,,,Attend.,Notes
+Tue Oct 22 2019,8:00p,New Orleans Pelicans,122,Toronto Raptors,130,Box Score,OT,20787,
+
+CREATE TABLE games
+(
+    id SERIAL UNIQUE PRIMARY KEY,
+    game_date VARCHAR(250) NOT NULL,
+    start_et VARCHAR(250) NOT NULL,
+    visitor VARCHAR(250) NOT NULL,
+    visitor_pts VARCHAR(250) NOT NULL,
+    home VARCHAR(250) NOT NULL,
+    home_pts VARCHAR(250) NOT NULL,
+    box_score_url VARCHAR(250) NOT NULL,
+    ot VARCHAR(250) NOT NULL,
+    Attend VARCHAR(250) NOT NULL,
+    Notes VARCHAR(250) NOT NULL
+);
+
+COPY games FROM '/publc/data/path_to_csv_file.csv' WITH FORMAT csv;
+
+# --- !Downs
+
+DROP TABLE games;
+
+DROP EXTENSION "uuid-ossp";
