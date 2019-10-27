15fc46c08f2f3f3ba9972addca59e1526fb004cd
Daniel Rogers <dcrogers127@gmail.com>
2019-10-27 10:23:10 -0700

    Add data

--- app/controllers/GameController.scala
+++ app/controllers/GameController.scala
@@ -1,8 +1,17 @@
 package controllers
+
 import play.api.mvc.{AbstractController, ControllerComponents}
+import play.api.libs.json.Json
 import security.UserAuthAction
+import services.ReadService
 
 class GameController(components: ControllerComponents,
                      userAuthAction: UserAuthAction, readService: ReadService) extends AbstractController(components) {
 
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+  def getGames = Action.async { implicit request =>
+    val gamesF = readService.getGames
+    gamesF.map { games => Ok(Json.toJson(games)) }
+  }
 }
