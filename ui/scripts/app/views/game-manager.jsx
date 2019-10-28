c5a446b01bb9977f5be9de76a0a9e0dff831f138
Daniel Rogers <dcrogers127@gmail.com>
2019-10-28 10:30:31 -0700

    Add data

--- ui/scripts/app/views/game-manager.jsx
+++ ui/scripts/app/views/game-manager.jsx
@@ -39,6 +39,38 @@ class GameManager extends React.Component {
   componentDidMount=()=>{
     axios.get("/api/games").then(this.handleResponse);
   };
+
+  render = () => {
+    const games = this.state.games;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
 export default GameManager;
