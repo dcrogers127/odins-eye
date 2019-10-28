2d8900ba522f5def45d2e07f5cac9b22064b0adb
Daniel Rogers <dcrogers127@gmail.com>
2019-10-28 10:25:27 -0700

    Add data

--- ui/scripts/app/views/game-manager.jsx
+++ ui/scripts/app/views/game-manager.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
 import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 class GameManager extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      games: []
+    };
+  };
+
+  const useStyles = makeStyles({
+    root: {
+      width: '100%',
+      overflowX: 'auto',
+    },
+    table: {
+      minWidth: 650,
+    },
+  });
+
+  handleResponse = (response) => {
+    if (response.status == 200) {
+      this.setState({
+        games: response.data
+      })
+    } else {
+      console.error(response.statusText);
+    }
+  }
+
+  componentDidMount=()=>{
+    axios.get("/api/games").then(this.handleResponse);
+  };
 }
 
 export default GameManager;
