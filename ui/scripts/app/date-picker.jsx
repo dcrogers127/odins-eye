14385553e3b84efa1d26ed51c473aed55e4af637
Daniel Rogers <dcrogers127@gmail.com>
2019-11-21 10:02:17 -0800

    Date picker.

---
+++ ui/scripts/app/date-picker.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class DatePicker extends React.Component {
+  state = {
+    startDate: new Date()
+  };
+
+  handleChange = date => {
+    this.setState({
+      startDate: date
+    });
+  };
+
+  render() {
+    return (
+      <DatePicker
+        selected={this.state.startDate}
+        onChange={this.handleChange}
+      />
+    );
+  }
+}
